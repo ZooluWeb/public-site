cf292379c54721c2c1b9ed4e37fb9c7d55b5aa0f
pbo <github@paulbouzakis>
2013-02-10 16:42:05 -0500

    Use express nodejs web framework.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 "use strict";
 
-var http = require('http')
 var port = process.env.PORT || 1337;
 
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+var express = require("expiress");
+var app = express();
+
+app.listen(port);
+console.log("Listening on port: " + port);
